9712e38e64e397b6345e9b2442bf6096bceb9551
Kourosh Talebzadeh <143129805+Kouroshkt@users.noreply.github.com>
2025-05-12 15:21:40 +0200

    Fixed send mailing on the EmailController (#8)
    Co-authored-by: Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>

--- src/main/java/org/iths/bookning/controller/EmailController.java
+++ src/main/java/org/iths/bookning/controller/EmailController.java
@@ -1,8 +1,11 @@
 package org.iths.bookning.controller;
 
 import org.iths.bookning.collection.NewsEmails;
+import org.iths.bookning.entities.EmailRequest;
 import org.iths.bookning.repositories.NewsEmailsRepository;
 import org.iths.bookning.services.EmailService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -14,9 +17,9 @@ public class EmailController {
     private final EmailService emailService;
     private final NewsEmailsRepository newsEmailsRepository;
 
-    public EmailController(EmailService emailService, NewsEmailsRepository newsEmailsRepository, NewsEmailsRepository newsEmailsRepository1) {
+    public EmailController(EmailService emailService, NewsEmailsRepository newsEmailsRepository) {
         this.emailService = emailService;
-        this.newsEmailsRepository = newsEmailsRepository1;
+        this.newsEmailsRepository = newsEmailsRepository;
     }
 
     @PostMapping("/sendnews")
@@ -29,4 +32,19 @@ public class EmailController {
             return "Fel vid skickning av e-post: " + e.getMessage();
         }
     }
+    @PostMapping("/send-confirmation")
+    public ResponseEntity<String> sendConfirmationEmail(@RequestBody EmailRequest emailRequest) {
+        try {
+            emailService.sendEmail(
+                    emailRequest.getEmail(),
+                    "Bekräftelse på bilbokning",
+                    emailRequest.getMessage()
+            );
+            return ResponseEntity.ok("E-post skickat till " + emailRequest.getEmail());
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Fel vid skickning av e-post: " + e.getMessage());
+        }
+    }
+
 }
