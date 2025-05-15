4456bc46bd373cce26f6d69d35b291496b8d25de
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2025-05-15 11:50:03 +0200

    Update carBookning table

--- src/main/java/org/iths/bookning/entities/CarBookning.java
+++ src/main/java/org/iths/bookning/entities/CarBookning.java
@@ -12,14 +12,19 @@ public class CarBookning {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToOne
-    @JoinColumn(name = "car_id")
+    @ManyToOne
+    @JoinColumn(name = "car_id", nullable = false)
     private Car car;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserInformation userInformation;
     private String startDate;
     private String endDate;
 
-    public CarBookning(Car car, String startDate, String endDate) {
+    public CarBookning(Car car, UserInformation userInformation, String startDate, String endDate) {
         this.car = car;
+        this.userInformation = userInformation;
         this.startDate = startDate;
         this.endDate = endDate;
     }
--- src/main/java/org/iths/bookning/repositories/CarBookningRepository.java
+++ src/main/java/org/iths/bookning/repositories/CarBookningRepository.java
@@ -1,10 +1,13 @@
 package org.iths.bookning.repositories;
 
 import org.iths.bookning.entities.CarBookning;
+import org.iths.bookning.entities.UserInformation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarBookningRepository extends CrudRepository<CarBookning, Long> {
     Iterable<CarBookning> findByCarId(Long carId);
+
+    Iterable<CarBookning> findByUserInformation(UserInformation user);
 }
